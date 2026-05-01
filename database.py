b5d53cf99890014fdb83a69567db9da79f1a25f8
James Holloway <james.holloway94@gmail.com>
2026-05-01 19:36:52 +0000

    publish lambda alias promotion marker after release

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-01 18:31:55 UTC
+# Last sync: 2026-05-01 19:36:52 UTC
