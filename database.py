a04dfe07df1398de39ba6c18e8bb9ead97680258
James Holloway <james.holloway94@gmail.com>
2026-05-05 10:30:50 +0000

    prime redis memory sync marker after flush

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-05 10:06:44 UTC
+# Last sync: 2026-05-05 10:30:50 UTC
