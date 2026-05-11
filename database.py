15936dcb28157d2b8880b153e39127e0855685e1
James Holloway <james.holloway94@gmail.com>
2026-05-11 12:33:15 +0000

    adjust redis ttl sync stamp after cache flush

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-11 11:55:30 UTC
+# Last sync: 2026-05-11 12:33:15 UTC
