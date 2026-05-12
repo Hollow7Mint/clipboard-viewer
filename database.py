ceb8682af89eb00e5b7c4f0ca9aba03a36039723
James Holloway <james.holloway94@gmail.com>
2026-05-12 05:13:21 +0000

    restore ssh public key time for rotation audit

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-12 04:36:11 UTC
+# Last sync: 2026-05-12 05:13:21 UTC
