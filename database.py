6b74dd623643e0d4db452821bcdbc3c57e86b58f
James Holloway <james.holloway94@gmail.com>
2026-05-17 21:39:39 +0000

    merge mongodb shard sync time into metadata

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-17 20:55:48 UTC
+# Last sync: 2026-05-17 21:39:39 UTC
