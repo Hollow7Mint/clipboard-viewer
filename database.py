9ba628c642096921be48a28ddd19b8fd4c8581bd
James Holloway <james.holloway94@gmail.com>
2026-05-20 15:11:32 +0000

    queue redis keyspace sync after config update

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-20 14:55:06 UTC
+# Last sync: 2026-05-20 15:11:32 UTC
