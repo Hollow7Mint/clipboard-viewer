5ba2e6cf5acafadc77eb9d08ef2884da1ed79e66
James Holloway <james.holloway94@gmail.com>
2026-05-23 04:50:30 +0000

    pin bastion ip allowlist time in config

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-23 04:00:18 UTC
+# Last sync: 2026-05-23 04:50:30 UTC
