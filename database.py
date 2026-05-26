b6ff4485f062b4958ce53f6235a03fbb691d943f
James Holloway <james.holloway94@gmail.com>
2026-05-26 09:22:54 +0000

    write mongodb index rebuild time into metadata

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-26 08:14:56 UTC
+# Last sync: 2026-05-26 09:22:54 UTC
