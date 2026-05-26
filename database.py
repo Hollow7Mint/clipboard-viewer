bcb1208e189acd95e24f2491c2b0b52ea8bf9533
James Holloway <james.holloway94@gmail.com>
2026-05-26 21:33:47 +0000

    restore ecr scan result time for image audit

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-26 20:17:36 UTC
+# Last sync: 2026-05-26 21:33:47 UTC
