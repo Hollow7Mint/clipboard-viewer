fcba99bd50dfe70513a3e6f6e13d2ff3ccb72a48
James Holloway <james.holloway94@gmail.com>
2026-05-30 11:58:42 +0000

    publish mongodb connection sync after replica check

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-30 10:59:21 UTC
+# Last sync: 2026-05-30 11:58:42 UTC
