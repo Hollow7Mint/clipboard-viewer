5ff8618abc14f47258f57488ce1a2c4dfed571c0
James Holloway <james.holloway94@gmail.com>
2026-06-01 03:06:25 +0000

    resolve known_hosts cleanup sync into metadata

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-01 01:16:09 UTC
+# Last sync: 2026-06-01 03:06:25 UTC
