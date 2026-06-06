8913676e61a673992e6b0c39bd0656af3cbf9e17
James Holloway <james.holloway94@gmail.com>
2026-06-06 08:14:52 +0000

    remap ec2 security group rule time in config

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-06 07:42:28 UTC
+# Last sync: 2026-06-06 08:14:52 UTC
