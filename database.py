8395a20982b99c8bd8525b7c4f2ca3d0da158d48
James Holloway <james.holloway94@gmail.com>
2026-06-07 18:02:16 +0000

    save postgres pool sync marker after refresh

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-07 16:32:31 UTC
+# Last sync: 2026-06-07 18:02:16 UTC
