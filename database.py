4cb5131f559e969eedaee4494760eb797f374566
James Holloway <james.holloway94@gmail.com>
2026-06-09 22:31:26 +0000

    frame mysql connection limit sync in config

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-09 21:59:39 UTC
+# Last sync: 2026-06-09 22:31:26 UTC
