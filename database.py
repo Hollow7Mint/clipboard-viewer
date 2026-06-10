a419ce32cbfae31e1c52a73117980ca726192327
James Holloway <james.holloway94@gmail.com>
2026-06-10 12:20:32 +0000

    restore mysql binlog time for replication audit

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-10 11:27:51 UTC
+# Last sync: 2026-06-10 12:20:32 UTC
