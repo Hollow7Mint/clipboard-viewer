c2d5544bbacf2256f885d1a73c6fe374ceea7399
James Holloway <james.holloway94@gmail.com>
2026-06-12 03:55:36 +0000

    normalize mysql charset sync time in metadata

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-12 02:53:47 UTC
+# Last sync: 2026-06-12 03:55:36 UTC
