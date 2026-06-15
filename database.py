c96abe411cb2cf595c3b5bda3603887591eab7a5
James Holloway <james.holloway94@gmail.com>
2026-06-15 14:06:43 +0000

    bridge ssh ProxyJump config sync through banner

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-15 12:49:50 UTC
+# Last sync: 2026-06-15 14:06:43 UTC
