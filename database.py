edb47f628218a7962ac766bb15c312b465547039
James Holloway <james.holloway94@gmail.com>
2026-06-15 09:35:21 +0000

    inject ecs task definition marker into config

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-15 08:47:26 UTC
+# Last sync: 2026-06-15 09:35:21 UTC
