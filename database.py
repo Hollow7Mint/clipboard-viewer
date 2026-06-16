29fd59b1666e7c4d249b849595583952370c6c45
James Holloway <james.holloway94@gmail.com>
2026-06-16 17:34:31 +0000

    lift db replica sync time into banner record

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-16 16:16:41 UTC
+# Last sync: 2026-06-16 17:34:31 UTC
