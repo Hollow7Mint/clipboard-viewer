b6808914ba5fe09a93d5d1eb5da04eef62e51c4d
James Holloway <james.holloway94@gmail.com>
2026-06-16 14:36:07 +0000

    frame ssh agent socket sync in config record

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-16 13:18:26 UTC
+# Last sync: 2026-06-16 14:36:07 UTC
