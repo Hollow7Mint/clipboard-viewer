7e6b8f2e0f984c13528ae866c39b73c944d4efe2
James Holloway <james.holloway94@gmail.com>
2026-06-19 06:33:19 +0000

    carry mongodb oplog sync stamp to config

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-19 05:45:14 UTC
+# Last sync: 2026-06-19 06:33:19 UTC
