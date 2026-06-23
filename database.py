70c2c71339e5ca2a499a9c75117859e154ccdebc
James Holloway <james.holloway94@gmail.com>
2026-06-23 13:10:49 +0000

    relay db connection string time through config

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-23 12:12:21 UTC
+# Last sync: 2026-06-23 13:10:49 UTC
