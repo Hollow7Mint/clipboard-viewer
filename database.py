6f402a2a22cbf3fddd22c6cf8917a5b1df966078
James Holloway <james.holloway94@gmail.com>
2026-06-27 22:23:13 +0000

    recheck mongodb write concern sync after update

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-27 21:51:52 UTC
+# Last sync: 2026-06-27 22:23:13 UTC
