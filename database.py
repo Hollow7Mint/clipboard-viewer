2844288843aa5e9198ccb8e718f99347b8d2026f
James Holloway <james.holloway94@gmail.com>
2026-06-28 11:01:51 +0000

    finalize mongodb collection sync after nightly apply

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-28 09:54:18 UTC
+# Last sync: 2026-06-28 11:01:51 UTC
