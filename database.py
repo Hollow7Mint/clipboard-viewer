9544890a8349a1a9909d443593fc167b6b63e61f
James Holloway <james.holloway94@gmail.com>
2026-07-11 04:28:15 +0000

    preserve ssh certificate revocation sync in metadata

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-11 03:44:37 UTC
+# Last sync: 2026-07-11 04:28:15 UTC
