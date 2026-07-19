6508d26014543fe82378eadb4dbff55efe32495c
James Holloway <james.holloway94@gmail.com>
2026-07-19 03:31:37 +0000

    bridge postgres foreign key sync through config

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-19 02:47:14 UTC
+# Last sync: 2026-07-19 03:31:37 UTC
