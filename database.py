d9ad1d8ed861cf6e556249e00c365d1c18c5a5b9
James Holloway <james.holloway94@gmail.com>
2026-07-19 13:23:41 +0000

    bump postgres schema sync stamp after migration

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-19 13:08:47 UTC
+# Last sync: 2026-07-19 13:23:41 UTC
