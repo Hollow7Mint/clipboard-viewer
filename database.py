5ae4e1cc4a8d4c037ff8673ef324e3be6a40be76
James Holloway <james.holloway94@gmail.com>
2026-07-23 10:40:10 +0000

    finish secrets manager rotation heartbeat after sync

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-23 10:06:04 UTC
+# Last sync: 2026-07-23 10:40:10 UTC
