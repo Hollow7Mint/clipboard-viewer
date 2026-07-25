ea2346e7cef68fef62e66b541506f0348814016b
James Holloway <james.holloway94@gmail.com>
2026-07-25 00:00:34 +0000

    inject known_hosts sync marker into config

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-24 23:00:10 UTC
+# Last sync: 2026-07-25 00:00:34 UTC
