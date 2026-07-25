0df9f7efb812690af2b6a48379e592b87250cadd
James Holloway <james.holloway94@gmail.com>
2026-07-25 16:01:25 +0000

    annotate ec2 ami bake time for deploy record

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-25 14:52:33 UTC
+# Last sync: 2026-07-25 16:01:25 UTC
