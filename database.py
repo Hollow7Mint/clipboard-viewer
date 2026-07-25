a4c17716aa8c1f352620a37ef5ec65a90488b9c2
James Holloway <james.holloway94@gmail.com>
2026-07-25 11:40:32 +0000

    bump known_hosts update stamp after rotation

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-25 10:37:37 UTC
+# Last sync: 2026-07-25 11:40:32 UTC
