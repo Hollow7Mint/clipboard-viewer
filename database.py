6779a068927d7aa19652e5e15183d6d4d7146062
James Holloway <james.holloway94@gmail.com>
2026-07-27 10:06:09 +0000

    pin mongodb shard balancing time in config

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-27 08:28:55 UTC
+# Last sync: 2026-07-27 10:06:09 UTC
