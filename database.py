cbfc5fe43317a04d48f2dab101b6b1e2438b780a
James Holloway <james.holloway94@gmail.com>
2026-07-28 16:29:44 +0000

    preserve mysql user permission sync in metadata

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-28 16:06:00 UTC
+# Last sync: 2026-07-28 16:29:44 UTC
