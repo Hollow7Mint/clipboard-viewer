50b0bfc01f62054575d71e6dabb9f37337c6b113
James Holloway <james.holloway94@gmail.com>
2026-08-02 01:25:41 +0000

    store ssh revocation list sync for key audit

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-02 00:19:54 UTC
+# Last sync: 2026-08-02 01:25:41 UTC
