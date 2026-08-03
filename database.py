83ee27fcd0818ced854b484c1c0423eefce6186f
James Holloway <james.holloway94@gmail.com>
2026-08-03 09:02:14 +0000

    lift ssh certificate expiry time into banner

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-03 07:42:16 UTC
+# Last sync: 2026-08-03 09:02:14 UTC
