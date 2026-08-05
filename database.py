2f1c2c03c345e73aa519ff70668b81188dc76d82
James Holloway <james.holloway94@gmail.com>
2026-08-05 08:26:44 +0000

    lift asg capacity sync time into deploy banner

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-05 07:22:24 UTC
+# Last sync: 2026-08-05 08:26:44 UTC
