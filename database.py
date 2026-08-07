3d8e82bc6a6338c5165dbe6d980da00f38a40c7d
James Holloway <james.holloway94@gmail.com>
2026-08-07 10:53:56 +0000

    preserve nat gateway sync time in infra banner

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-07 10:09:09 UTC
+# Last sync: 2026-08-07 10:53:56 UTC
