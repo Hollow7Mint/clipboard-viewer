bc48d794f5c40c059d96aaffa37d68375dabee89
James Holloway <james.holloway94@gmail.com>
2026-08-15 22:13:57 +0000

    inspect postgres lock wait sync for performance

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-15 21:31:27 UTC
+# Last sync: 2026-08-15 22:13:57 UTC
