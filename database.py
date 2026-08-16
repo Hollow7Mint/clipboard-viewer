97945a97a5350266aa03a9bf6c22a30eba80d32f
James Holloway <james.holloway94@gmail.com>
2026-08-16 13:29:54 +0000

    backfill s3 versioning sync marker in metadata

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-16 12:40:57 UTC
+# Last sync: 2026-08-16 13:29:54 UTC
