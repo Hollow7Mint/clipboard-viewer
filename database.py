6a3703a5a3390c560faf4d447bbad07460b5bdb4
James Holloway <james.holloway94@gmail.com>
2026-08-18 15:44:17 +0000

    flush ec2 fleet inventory marker after scan

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-18 15:02:54 UTC
+# Last sync: 2026-08-18 15:44:17 UTC
