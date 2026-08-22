8995cd20ec35a7b62b7c3054c83594f2a377c5cb
James Holloway <james.holloway94@gmail.com>
2026-08-22 22:59:07 +0000

    seal postgres logical replication sync in banner

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-22 21:14:02 UTC
+# Last sync: 2026-08-22 22:59:06 UTC
