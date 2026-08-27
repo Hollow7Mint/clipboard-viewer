6fb9bbe22d887edfe2b3710a448be5cf0cfa47cf
James Holloway <james.holloway94@gmail.com>
2026-08-27 09:12:24 +0000

    flush mongodb replica lag marker for nightly check

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-27 08:50:27 UTC
+# Last sync: 2026-08-27 09:12:24 UTC
