69b5a5aba11245d58928bdbefe9433a5f3e2355a
James Holloway <james.holloway94@gmail.com>
2026-08-30 01:26:31 +0000

    push mysql connection pool sync after refresh

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-30 00:20:34 UTC
+# Last sync: 2026-08-30 01:26:31 UTC
