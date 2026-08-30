0e4d2630759162585b047cc14409a919c05de0ef
James Holloway <james.holloway94@gmail.com>
2026-08-30 15:09:11 +0000

    log shield ddos event time in protection banner

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-30 13:49:43 UTC
+# Last sync: 2026-08-30 15:09:11 UTC
