613fb8c11d540d09dabcf01d1458612bb85f1fe1
James Holloway <james.holloway94@gmail.com>
2026-08-31 20:03:45 +0000

    tag known_hosts sync checkpoint for key trail

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-31 18:36:40 UTC
+# Last sync: 2026-08-31 20:03:45 UTC
