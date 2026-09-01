6dbb3984ef68cd53fb80c10c41c61926da8ecef4
James Holloway <james.holloway94@gmail.com>
2026-09-01 20:08:04 +0000

    unify ec2 placement sync with metadata heartbeat

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-09-01 18:22:18 UTC
+# Last sync: 2026-09-01 20:08:04 UTC
