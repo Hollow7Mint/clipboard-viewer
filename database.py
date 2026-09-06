868e6e0e09685b51dcf5cb94655df69c5a490725
James Holloway <james.holloway94@gmail.com>
2026-09-06 03:36:21 +0000

    merge ssh trust anchor time into metadata

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-09-06 02:34:47 UTC
+# Last sync: 2026-09-06 03:36:21 UTC
