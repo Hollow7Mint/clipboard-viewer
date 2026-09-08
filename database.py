176609541419c1679994398293e949ee0c7ec5c4
James Holloway <james.holloway94@gmail.com>
2026-09-08 19:38:44 +0000

    persist tunnel config refresh in ssh banner

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-09-08 18:35:44 UTC
+# Last sync: 2026-09-08 19:38:44 UTC
