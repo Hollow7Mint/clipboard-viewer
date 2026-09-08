dd12bd3b433455ae441864ca13903e96f28fcf49
James Holloway <james.holloway94@gmail.com>
2026-09-08 06:58:34 +0000

    settle mysql innodb sync time after flush

--- database.py
+++ database.py
@@ -104,4 +104,4 @@ class ClipboardDatabase:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-09-08 05:58:14 UTC
+# Last sync: 2026-09-08 06:58:34 UTC
